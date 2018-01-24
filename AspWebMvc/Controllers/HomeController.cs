8a984ff25785da8a52818312acf89fba3f2a4eac
Ron Nims <rleenims@gmail.com>
2018-01-24 11:01:20 -0800

    First commit AspWebMvc

--- AspWebMvc/Controllers/HomeController.cs
+++ AspWebMvc/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AspWebMvc.Models;
 
 namespace AspWebMvc.Controllers
 {
@@ -10,7 +11,9 @@ namespace AspWebMvc.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+
+            return View(db.GrantTypes.ToList());
         }
 
         public ActionResult About()
